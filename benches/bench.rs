3c79dfb380e924f735789ed546e0cf038c39fde4
Raj Mehta <rcmehta@hotmail.co.uk>
2020-12-22 23:07:14 +0000

    Fixed random generation of vectors on sphere and in disc, benchmarked variations

--- benches/bench.rs
+++ benches/bench.rs
@@ -3,32 +3,57 @@ use criterion::{criterion_group, criterion_main, Criterion};
 extern crate raytracer;
 use raytracer::vec3::*;
 
-fn random_by_gaussian() -> Vec3 {
+// For Vec3::random_on_unit_sphere()
+
+fn random_on_by_gaussian() -> Vec3 {
     loop {
         let vec = Vec3::new(random_gaussian(), random_gaussian(), random_gaussian());
-        if vec.x() != 0.0 && vec.y() != 0.0 && vec.z() != 0.0 {
-            return vec / vec.length();
+        if vec.length_squared() > 0.0 {
+            return vec.unit();
         }
     }
 }
 
-fn random_by_select() -> Vec3 {
+fn random_on_by_rejection() -> Vec3 {
     loop {
         let vec = Vec3::random_vector(-1.0, 1.0);
-        if vec.length_squared() < 1.0 {
-            return vec / vec.length();
+        if vec.length_squared() > 0.0 && vec.length_squared() <= 1.0{
+            return vec.unit();
         }
+    }
+}
 
+// For Vec3::random_in_unit_disc().
+fn random_in_by_gaussian() -> Vec3 {
+    loop {
+        let vec = Vec3::new(random_gaussian(), random_gaussian(), 0.0);
+        let r = random().powf(0.5);
+        if vec.length_squared() > 0.0 {
+            return vec.unit() * r;
+        }
+    }
+}
+
+fn random_in_by_rejection() -> Vec3 {
+    loop {
+        let vec = Vec3::new(random_range(-1.0, 1.0), random_range(-1.0, 1.0), 0.0);
+        if vec.length_squared() < 1.0 {
+            return vec;
+        }
     }
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("gaussian", |b| b.iter(|| random_by_gaussian()));
-    c.bench_function("select", |b| b.iter(|| random_by_select()));
+    c.bench_function("on_gaussian", |b| b.iter(|| random_on_by_gaussian()));
+    c.bench_function("on_rejection", |b| b.iter(|| random_on_by_rejection()));
+
+    c.bench_function("in_gaussian", |b| b.iter(|| random_in_by_gaussian()));
+    c.bench_function("in_rejection", |b| b.iter(|| random_in_by_rejection()));
 }
 
 criterion_group!(benches, criterion_benchmark);
 criterion_main!(benches);
 
-// Gaussian - 70ns
-// Select - 110ns
+// Type - On, In
+// Gaussian ~ 68 ns, 63 ns
+// Rejection ~ 104 ns, 46 ns - curse of dimensionality
